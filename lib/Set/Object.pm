3b274076e17add95f6ca7b875cd65ccdd0931402
samv <samv>
2005-08-30 02:09:52 +0000

    Release 1.12

--- lib/Set/Object.pm
+++ lib/Set/Object.pm
@@ -364,10 +364,9 @@ require AutoLoader;
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
 
-@EXPORT = qw(set);
 @EXPORT_OK = qw( ish_int is_int is_string is_double blessed reftype
-		 refaddr is_overloaded is_object is_key );
-$VERSION = '1.11_02';
+		 refaddr is_overloaded is_object is_key set );
+$VERSION = '1.12';
 
 bootstrap Set::Object $VERSION;
 
