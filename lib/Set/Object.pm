b782ff62081fed59fedddc5cf3469f4878954263
Sam Vilain <sam@vilain.net>
2005-09-27 18:13:09 -0800

    import Set-Object 1.14 from CPAN
    git-cpan-module:   Set-Object
    git-cpan-version:  1.14
    git-cpan-authorid: SAMV
    git-cpan-file:     authors/id/S/SA/SAMV/Set-Object-1.14.tar.gz

--- lib/Set/Object.pm
+++ lib/Set/Object.pm
@@ -366,7 +366,7 @@ require AutoLoader;
 
 @EXPORT_OK = qw( ish_int is_int is_string is_double blessed reftype
 		 refaddr is_overloaded is_object is_key set );
-$VERSION = '1.13';
+$VERSION = '1.14';
 
 bootstrap Set::Object $VERSION;
 
