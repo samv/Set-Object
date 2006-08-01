a09716007a2496b8a94998c3d2bd7fb4c4561d26
Sam Vilain <sam.vilain@catalyst.net.nz>
2006-08-02 10:44:18 +1200

    Release engineering for 1.18

--- lib/Set/Object.pm
+++ lib/Set/Object.pm
@@ -415,7 +415,7 @@ require AutoLoader;
 
 @EXPORT_OK = qw( ish_int is_int is_string is_double blessed reftype
 		 refaddr is_overloaded is_object is_key set );
-$VERSION = '1.17';
+$VERSION = '1.18';
 
 bootstrap Set::Object $VERSION;
 
