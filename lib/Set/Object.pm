b9b8b582edf24cb2fe0d49f293bc0012b64f3d5a
Sam Vilain <sam.vilain@catalyst.net.nz>
2007-01-23 14:01:40 +1300

    Set::Object 1.19 release engineering

--- lib/Set/Object.pm
+++ lib/Set/Object.pm
@@ -439,7 +439,7 @@ require AutoLoader;
 
 @EXPORT_OK = qw( ish_int is_int is_string is_double blessed reftype
 		 refaddr is_overloaded is_object is_key set weak_set );
-$VERSION = '1.18';
+$VERSION = '1.19';
 
 bootstrap Set::Object $VERSION;
 
