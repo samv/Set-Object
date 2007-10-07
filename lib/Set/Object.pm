17f8ac3ad099576db6c39095848552714a9520f5
Sam Vilain <sam.vilain@catalyst.net.nz>
2007-10-08 12:06:42 +1300

    [releng] Set::Object 1.22

--- lib/Set/Object.pm
+++ lib/Set/Object.pm
@@ -497,7 +497,7 @@ require AutoLoader;
 
 @EXPORT_OK = qw( ish_int is_int is_string is_double blessed reftype
 		 refaddr is_overloaded is_object is_key set weak_set );
-$VERSION = '1.21';
+$VERSION = '1.22';
 
 bootstrap Set::Object $VERSION;
 
