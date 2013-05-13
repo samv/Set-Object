eaf433531e0a3565ae46625770118548b640dd31
Reini Urban <rurban@cpanel.net>
2013-05-13 15:32:43 -0500

    1.31: fix RT#85244, Changes.pod patch by Gregor Hermann

--- lib/Set/Object.pm
+++ lib/Set/Object.pm
@@ -524,7 +524,7 @@ require AutoLoader;
 
 @EXPORT_OK = qw( ish_int is_int is_string is_double blessed reftype
 		 refaddr is_overloaded is_object is_key set weak_set );
-$VERSION = '1.30';
+$VERSION = '1.31';
 
 bootstrap Set::Object $VERSION;
 
