aead29ea50ef91e071de5f745c51fd15b2f3ddc8
Reini Urban <rurban@cpanel.net>
2013-12-13 20:34:26 -0600

    1.32: Revert Changes.pod patch RT#85244
    It conflicts with the Set::Object pod on metacpan

--- lib/Set/Object.pm
+++ lib/Set/Object.pm
@@ -524,7 +524,7 @@ require AutoLoader;
 
 @EXPORT_OK = qw( ish_int is_int is_string is_double blessed reftype
 		 refaddr is_overloaded is_object is_key set weak_set );
-$VERSION = '1.31';
+$VERSION = '1.32';
 
 bootstrap Set::Object $VERSION;
 
