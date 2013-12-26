af7b34f6080a077a57ad0d6c920c517676cf15f8
Reini Urban <rurban@cpanel.net>
2013-12-25 19:55:17 -0600

    1.34: bump version

--- lib/Set/Object.pm
+++ lib/Set/Object.pm
@@ -526,7 +526,7 @@ require AutoLoader;
 
 @EXPORT_OK = qw( ish_int is_int is_string is_double blessed reftype
 		 refaddr is_overloaded is_object is_key set weak_set );
-$VERSION = '1.33';
+$VERSION = '1.34';
 
 bootstrap Set::Object $VERSION;
 
